733307e676613ade00d2ad10c091c4de3f5c438b
blubrackets <6eckermans2015@gmail.com>
2016-03-02 12:29:37 -0600

    Version 0.0.2 release and README update.

--- lib/dime.rb
+++ lib/dime.rb
@@ -1,6 +1,16 @@
 class Dime
-	def self.in(prompt)
+	def self.sin(prompt) #Standard user input from console..
 		print prompt
 		return gets.chomp
+	end
+
+	def self.cin(name) #Command line input..
+		unless ARGV.length do == 1
+			puts "Error: expected argument #{name}."
+			name = name.upcase
+			script_name = __FILE__
+			puts "Usage: ruby #{script_name} [#{name}]"
+		end
+		return ARGV[0]
 	end
 end
